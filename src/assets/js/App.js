232583dc030bef218d5b78fecb6f8527b0c885ff
Poylar <doalesk8@gmail.com>
2023-02-14 20:03:05 +0800

    deploy

--- src/assets/js/App.js
+++
@@ -1,8 +0,0 @@
-import './Sliders';
-import './Tabs';
-import './Faq';
-import './CustomCursor';
-import './Video';
-import './Modal';
-import './Archon';
-import './ShowCases';
--- src/assets/js/Archon.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    document.querySelector(e.currentTarget.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-    });
-  });
-});
